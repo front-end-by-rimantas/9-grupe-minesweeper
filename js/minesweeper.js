d53f06827ecb3fefd5391c4196fa1178aa2a9a97
Rimantas <belauzas@yahoo.com>
2019-08-05 12:57:48 +0300

    Final commit

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -9,14 +9,19 @@ class Minesweeper {
         this.clock;
 
         // inner object logic / memory
+        this.bombCountStart = 80;
         this.board = {
             rows: 20,
             columns: 30
         };
         this.timer;
         this.seconds = 0;
-        this.bombCount = 80;
-        this.map = [];                   // pvz.: [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+        this.bombCount = 0;
+        this.map = [];                  // reiksmes: 0 - nera bombos,
+                                        // 1 - yra bomba,
+                                        // 2 - atidarytas langelis,
+                                        // 8 - flag ant bombos,
+                                        // 9 - flag ant tuscio langelio
         this.bombs = [];
         this.gameStatus = 'start';       // start / in-progress / end
         this.gameEndType = null;         // happy / death
@@ -39,9 +44,10 @@ class Minesweeper {
 
     init = () => {
         // game reset
-        this.map = [];
+        this.generateEmptyBombs();
         this.bombs = [];
-        this.timer = null;
+        this.bombCount = this.bombCountStart;
+        clearInterval(this.timer);
         this.seconds = 0;
         this.gameStatus = 'start';
         this.gameEndType = null;
@@ -56,14 +62,7 @@ class Minesweeper {
         this.bombCounter = this.target.querySelector('.bombs');
         this.clock = this.target.querySelector('.timer');
 
-        let bombCount = '';
-        if ( this.bombCount < 100 ) {
-            bombCount += '0';
-        }
-        if ( this.bombCount < 10 ) {
-            bombCount += '0';
-        }
-        this.bombCounter.textContent = bombCount + this.bombCount;
+        this.bombDisplay();
         
         this.clock.textContent = '000';
 
@@ -82,16 +81,62 @@ class Minesweeper {
         // kiekvienai cell'ei uzdedame click event'a
         this.target.querySelectorAll('section > .cell').forEach(cell => {
             cell.addEventListener('click', this.cellClick);
+            cell.addEventListener('contextmenu', this.toggleFlag);
         });
 
         // smile click event
         this.smile.addEventListener('click', this.init);
     }
 
+    bombDisplay = () => {
+        let bombCount = '';
+        if ( this.bombCount < 100 ) {
+            bombCount += '0';
+        }
+        if ( this.bombCount < 10 ) {
+            bombCount += '0';
+        }
+        this.bombCounter.textContent = bombCount + this.bombCount;
+    }
+
+    toggleFlag = ( event ) => {
+        event.preventDefault();
+        
+        if ( this.gameStatus === 'end' ) {
+                return;
+        }
+
+        const cellId = event.target.dataset.id;
+        const x = cellId % this.board.columns;
+        const y = (cellId - x) / this.board.columns;
+
+        if ( this.map[y][x] === 2 ) {
+            return;
+        }
+        
+        if ( this.map[y][x] === 8 || this.map[y][x] === 9 ) {
+            event.target.classList.remove('flag');
+            this.bombCount++;
+            if ( this.map[y][x] === 8 ) this.map[y][x] = 1;
+            if ( this.map[y][x] === 9 ) this.map[y][x] = 0;
+        } else {
+            event.target.classList.add('flag');
+            this.bombCount--;
+            if ( this.map[y][x] === 1 ) this.map[y][x] = 8;
+            if ( this.map[y][x] === 0 ) this.map[y][x] = 9;
+        }
+
+        this.bombDisplay();
+    }
+
     cellClick = ( event ) => {
         const cellId = parseInt(event.target.dataset.id);
         const x = cellId % this.board.columns;
         const y = (cellId - x) / this.board.columns;
+
+        if ( this.map[y][x] === 8 || this.map[y][x] === 9 ) {
+            return;
+        }
 
         if ( this.gameStatus === 'start' ) {
             this.gameStatus = 'in-progress';
@@ -147,8 +192,9 @@ class Minesweeper {
                         // reikia sustabdyti laikrodi
                         clearInterval( this.timer );
 
-                        // pakeisti smile
-                        this.smile.textContent = 'B-)';
+                        // atnaujiname zaidimo statusa
+                        this.gameStatus = 'end';
+                        this.gameEndType = 'happy';
                     }
             }
         }
@@ -181,14 +227,14 @@ class Minesweeper {
         // this.map pazymime, kad langelis jau atidarytas
         this.map[y][x] = 2;
 
-        if ( y > 0 && x > 0 && this.map[y-1][x-1] === 1 ) { count++ }     // top left
-        if ( y > 0 && this.map[y-1][x] === 1 ) { count++ }     // top
-        if ( y > 0 && x < col && this.map[y-1][x+1] === 1 ) { count++ }     // top right
-        if ( x < col && this.map[y][x+1] === 1 ) { count++ }     // right
-        if ( y < row && x < col && this.map[y+1][x+1] === 1 ) { count++ }     // bottom right
-        if ( y < row && this.map[y+1][x] === 1 ) { count++ }     // bottom
-        if ( y < row && x > 0 && this.map[y+1][x-1] === 1 ) { count++ }     // bottom left
-        if ( x > 0 && this.map[y][x-1] === 1 ) { count++ }     // left
+        if ( y > 0 && x > 0 && (this.map[y-1][x-1] === 1 || this.map[y-1][x-1] === 8) ) { count++ }     // top left
+        if ( y > 0 && (this.map[y-1][x] === 1 || this.map[y-1][x] === 8) ) { count++ }     // top
+        if ( y > 0 && x < col && (this.map[y-1][x+1] === 1 || this.map[y-1][x+1] === 8) ) { count++ }     // top right
+        if ( x < col && (this.map[y][x+1] === 1 || this.map[y][x+1] === 8) ) { count++ }     // right
+        if ( y < row && x < col && (this.map[y+1][x+1] === 1 || this.map[y+1][x+1] === 8) ) { count++ }     // bottom right
+        if ( y < row && (this.map[y+1][x] === 1 || this.map[y+1][x] === 8) ) { count++ }     // bottom
+        if ( y < row && x > 0 && (this.map[y+1][x-1] === 1 || this.map[y+1][x-1] === 8) ) { count++ }     // bottom left
+        if ( x > 0 && (this.map[y][x-1] === 1 || this.map[y][x-1] === 8) ) { count++ }     // left
 
         // i ta langeli irasau bombu skaiciu (pridedame class="n-[bombuSkaicius]")
         this.target.querySelector(`.cell[data-id="${cellId}"]`).classList.add('n-'+count);
@@ -210,12 +256,24 @@ class Minesweeper {
         }
     }
 
+    // zaidimo pradziai reikia tuscio zemelapio
+    generateEmptyBombs = () => {
+        this.map = [];
+
+        for ( let r=0; r<this.board.rows; r++ ) {
+            this.map.push([]);
+            for ( let c=0; c<this.board.columns; c++ ) {
+                this.map[r].push(0);
+            }
+        }
+    }
+
     generateBombs = ( exclude ) => {
         let bombList = [],
             position = 0,
             board = this.board.columns * this.board.rows - 1;
         
-        while ( bombList.length !== this.bombCount ) {
+        while ( bombList.length !== this.bombCountStart ) {
             position = Math.round(Math.random() * board);
             if ( bombList.indexOf(position) === -1 &&
                  position !== exclude ) {
@@ -226,16 +284,14 @@ class Minesweeper {
         this.bombs = bombList;
         
         // sugeneruojame 2D bombu zemelapio masyva
-        this.map = []
+        for ( let b=0; b<this.bombCountStart; b++ ) {
+            const x = bombList[b] % this.board.columns;
+            const y = (bombList[b] - x) / this.board.columns;
 
-        for ( let r=0; r<this.board.rows; r++ ) {
-            this.map.push([]);
-            for ( let c=0; c<this.board.columns; c++ ) {
-                if ( bombList.indexOf( r * this.board.columns + c ) >= 0 ) {
-                    this.map[r].push(1);
-                } else {
-                    this.map[r].push(0);
-                }
+            if ( this.map[y][x] === 9 ) {
+                this.map[y][x] = 8;
+            } else {
+                this.map[y][x] = 1;
             }
         }
 
@@ -250,10 +306,9 @@ class Minesweeper {
         this.map.forEach( row => {
             
             row.forEach( cell => {
-                if ( cell === 0 ) leftToOpen++;
+                if ( cell === 0 || cell === 9 ) leftToOpen++;
             })
         })
-        console.log(this.map);
         
         return leftToOpen > 0 ? false : true;
     }
